5c8db92c412dd23eed9330ee46554ffb1f1863d3
crashoverride <yussif.muhaimin4@gmail.com>
2022-08-27 00:56:46 +0000

    Introduced to Python OOP and how to install and use external packages from Python library.

---
+++ day-16-start/main.py
@@ -0,0 +1,22 @@
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.color("coral")
+# timmy.shape("turtle")
+# timmy.forward(100)
+# timmy.bk(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+from prettytable import PrettyTable
+
+
+table = PrettyTable()
+table.add_column("Pokemon Name",
+                 ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type",
+                 ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
