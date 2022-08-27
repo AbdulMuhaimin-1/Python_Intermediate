711eeca90ec265740e51ed3c3ea3a007a9a9df1c
crashoverride <yussif.muhaimin4@gmail.com>
2022-08-27 00:53:08 +0000

    Followed lesson on how to create and use classes, class Objects, methods and attributes.

---
+++ quiz-game-start/data.py
@@ -0,0 +1,40 @@
+question_data = [
+        {
+            "category": "Science: Computers",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Linus Torvalds created Linux and Git.",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]
+        },
+        {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+         "question": "The logo for Snapchat is a Bell.", "correct_answer": "False", "incorrect_answers": ["True"]},
+        {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+         "question": "RAM stands for Random Access Memory.", "correct_answer": "True", "incorrect_answers": ["False"]},
+        {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+         "question": "Ada Lovelace is often considered the first computer programmer.", "correct_answer": "True",
+         "incorrect_answers": ["False"]},
+        {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+                                           "question": "&quot;HTML&quot; stands for Hypertext Markup Language.",
+                                           "correct_answer": "True", "incorrect_answers": ["False"]},
+        {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+         "question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
+         "correct_answer": "True", "incorrect_answers": ["False"]},
+        {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+         "question": "Time on Computers is measured via the EPOX System.", "correct_answer": "False",
+         "incorrect_answers": ["True"]},
+        {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+                                          "question": "The Windows 7 operating system has six main editions.",
+                                          "correct_answer": "True", "incorrect_answers": ["False"]},
+        {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+         "question": "Linux was first created as an alternative to Windows XP.", "correct_answer": "False",
+         "incorrect_answers": ["True"]},
+        {
+            "category": "Science: Computers",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]}
+    ]
+
