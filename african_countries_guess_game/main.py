49627c2ad7cb942230c5643599a2fc8603d0b783
crashoverride <yussif.muhaimin4@gmail.com>
2022-10-22 13:40:45 +0000

    continue project on countries guess game

--- african_countries_guess_game/main.py
+++ african_countries_guess_game/main.py
@@ -1,28 +1,17 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 img = "africa_map.gif"
 screen.addshape(img)
 turtle.shape(img)
 
-countries = ["Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cabo Verde", "Cameroon", "Central",
-             "African Republic (CAR)", "Chad", "Comoros", "Democratic Republic of the Congo",
-             "Republic of the Congo, Cote d'Ivoire", "Djibouti", "Egypt", "Equatorial Guinea", "Eritrea", "Eswatini",
-             "Ethiopia", "Gabon", "Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Kenya", "Lesotho", "Liberia", "Libya",
-             "Madagascar", "Malawi", "Mali", "Mauritania", "Mauritius", "Morocco", "Mozambique", "Namibia", "Niger",
-             "Nigeria", "Rwanda", "Sao Tome and Principe", "Senegal", "Seychelles", "Sierra Leone", "Somalia",
-             "South Africa", "South Sudan", "Sudan", "Tanzania", "Togo", "Tunisia", "Uganda", "Zambia", "Zimbabwe"]
-x = []
-y = []
+country_df = pandas.read_csv("country_coord.csv")
+print(country_df.loc[3])
 
 answer = screen.textinput(title="Guess The Turtle", prompt="Enter the country to be guessed:")
 
 
-def get_coordinates(x_cor, y_cor):
-    print(f"X = {x_cor}, Y = {y_cor}")
-
-
-turtle.onscreenclick(get_coordinates)
 
 
 screen.mainloop()
