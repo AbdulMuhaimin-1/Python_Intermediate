bacae23d6b7d9badf46b0295ef5ce8e012c12a23
u551f <yussif.muhaimin4@gmail.com>
2022-11-09 10:10:22 +0000

    complete for now

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -87,7 +87,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=200, height=200)
 lock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_img)
-canvas.grid(column=1, row=0)
+canvas.grid(column=0, row=0, columnspan=3)
 
 # Labels
 web_label = Label(text="Website:")
