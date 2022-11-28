75b80540bdac244a8a7a0ad8a3343f38786c226b
u551f <yussif.muhaimin4@gmail.com>
2022-11-28 18:20:22 +0000

    Added new funcitionalities

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -2,12 +2,15 @@ from tkinter import *
 import pandas
 import random
 BACKGROUND_COLOR = "#B1DDC6"
-
-# ------------------------------GENERATE WORDS -----------------------------#
-
-df = pandas.read_csv('data/french_words.csv')
-to_learn = df.to_dict(orient='records')
 current_card = {}
+# ------------------------------GENERATE WORDS -----------------------------#
+try:
+    data = pandas.read_csv('data/words_to_learn.csv')
+except FileNotFoundError:
+    original_data = pandas.read_csv('data/french_words.csv')
+    to_learn = original_data.to_dict(orient='records')
+else:
+    to_learn = data.to_dict(orient='records')
 
 
 def generate_words():
@@ -19,19 +22,19 @@ def generate_words():
     canvas.itemconfig(dialect_text, text=current_card['French'], fill='black')
     flip_timer = window.after(3000, func=flip_card)
 
-    to_learn.remove(current_card)
-
-def words_to_save():
-    # with open('words_to_learn.csv', 'w') as updated_data:
-    df.to_csv('words_to_learn.csv', index=False)
-
-
-
 
 def flip_card():
     canvas.itemconfig(card_background, image=card_back_image)
     canvas.itemconfig(card_title, text='English', fill='white')
     canvas.itemconfig(dialect_text, text=current_card['English'], fill='white')
+
+
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    new_list = pandas.DataFrame(to_learn)
+    new_list.to_csv('data/words_to_learn.csv', index=False)
+    generate_words()
 
 
 # ---------------------------- UI SETUP ---------------------------------#
@@ -52,11 +55,11 @@ canvas.grid(column=0, row=0, columnspan=2)
 
 # Buttons
 my_right_image_button = PhotoImage(file="./images/right.png")
-right_button = Button(image=my_right_image_button, highlightthickness=0, command=generate_words)
+right_button = Button(image=my_right_image_button, highlightthickness=0, command=is_known)
 right_button.grid(column=1, row=1)
 
 my_wrong_image_button = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=my_wrong_image_button, highlightthickness=0, command=words_to_save)
+wrong_button = Button(image=my_wrong_image_button, highlightthickness=0, command=generate_words)
 wrong_button.grid(column=0, row=1)
 
 generate_words()
