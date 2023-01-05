b7b0db7b0755de9d87d3a909decec2ed6708d324
u551f <yussif.muhaimin4@gmail.com>
2023-01-05 03:47:49 +0000

    Modified day-17 quiz project the GUI

---
+++ api_quizzler_app_project/ui.py
@@ -0,0 +1,67 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title('Quizzler')
+        self.window.config(padx=20, pady=20, background=THEME_COLOR)
+
+        self.score_text = Label(text='Score: 0', foreground='white', background=THEME_COLOR)
+        self.score_text.grid(row=0, column=1)
+
+        self.canvas = Canvas(width= 300, height=250, bg='white')
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="The teeth is naturally yellowish.",
+            font=('Arial', 14, 'italic'),
+            fill=THEME_COLOR
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file='images/true.png')
+        self.true_button = Button(image=true_img, highlightthickness=0, command=self.true_answer)
+        self.true_button.grid(row=2, column=0)
+
+        false_img = PhotoImage(file='images/false.png')
+        self.false_button = Button(image=false_img, highlightthickness=0, command=self.false_answer)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg='white')
+        if self.quiz.still_has_questions():
+            self.score_text.config(text=f'Score: {self.quiz.score}')
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text='You\'ve reached the end of the questions.')
+            self.true_button.config(state='disabled')
+            self.false_button.config(state='disabled')
+
+    def true_answer(self):
+        is_right = self.quiz.check_answer('True')
+        self.give_feedback(is_right)
+
+    def false_answer(self):
+        is_right = self.quiz.check_answer('False')
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg='green')
+        else:
+            self.canvas.config(bg='red')
+        self.window.after(1000, self.get_next_question)
+
+
