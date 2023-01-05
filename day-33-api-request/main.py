809b96ff792e840df6f4f75492b87cfbdb393fd4
u551f <yussif.muhaimin4@gmail.com>
2023-01-05 03:46:08 +0000

    Updated

--- day-33-api-request/main.py
+++ day-33-api-request/main.py
@@ -1,13 +1,33 @@
 import requests
+from datetime import datetime
+MY_LAT = 5.696800
+MY_LONG = -0.024960
+OFF = 0
+# response = requests.get(url='http://api.open-notify.org/iss-now.json')
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data['iss_position']['longitude']
+# latitude = data['iss_position']['latitude']
+#
+# iss_position = (longitude, latitude)
+#
+# print(iss_position)
 
-response = requests.get(url='http://api.open-notify.org/iss-now.json')
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": OFF,
+}
+
+response = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
 response.raise_for_status()
-
 data = response.json()
+sunrise = data['results']['sunrise'].split('T')[1].split(':')[0]
+sunset = data['results']['sunset'].split('T')[1].split(':')[0]
+print(sunrise)
+print(sunset)
 
-longitude = data['iss_position']['longitude']
-latitude = data['iss_position']['latitude']
-
-iss_position = (longitude, latitude)
-
-print(iss_position)
+time_now = datetime.now()
+print(time_now.hour)
